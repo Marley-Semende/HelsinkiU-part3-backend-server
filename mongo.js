7ab2157e5ca85de1316586191c45de79d1d1f598
Marley-Semende <marley.semende@gmail.com>
2024-06-18 23:33:27 +0200

    add initial mongdb database

--- mongo.js
+++ mongo.js
@@ -1,38 +1,8 @@
 import mongoose, { Schema, connect } from "mongoose";
-import mongodb from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
 
-if (process.argv.length < 3) {
-  console.log("give password, name and phone number as argument");
-  process.exit(1);
-}
+const MONGO_URI = process.env.MONGO_URI;
 
-const password = process.argv[2];
-const name = process.argv[3];
-const phoneNumber = process.argv[4];
-
-const URI = `mongodb+srv://marleysemende:${password}@cluster4.3asachz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster4`;
-
-mongoose.connect(URI);
+mongoose.connect(MONGO_URI);
 console.log("Connected to MongoDB database successfully!");
-
-const personSchema = new mongoose.Schema({
-  name: String,
-  phoneNumber: String,
-});
-
-const Person = mongoose.model("Person", personSchema);
-
-const person = new Person({
-  name,
-  phoneNumber,
-});
-
-person.save();
-console.log(`${person} saved`);
-
-Person.find({}).then((result) => {
-  result.forEach((person) => {
-    console.log(person);
-  });
-  mongoose.connection.close();
-});
