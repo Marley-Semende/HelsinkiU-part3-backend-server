efca28de62155a34830d816a64c4d70545ca958d
Marley-Semende <marley.semende@gmail.com>
2024-06-18 23:47:50 +0200

    converted id to string

--- mongo.js
+++ mongo.js
@@ -17,3 +17,11 @@ const personSchema = new mongoose.Schema({
   name: String,
   phoneNumber: String,
 });
+
+personSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
