b6cb50b66640948d4b825aa6ae7543fe32ce5f28
Marley-Semende <marley.semende@gmail.com>
2024-06-19 02:02:57 +0200

    Add Person model

--- models/person.js
+++ models/person.js
@@ -1,4 +1,4 @@
-import mongoose, { Schema, connect } from "mongoose";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -6,17 +6,20 @@ const MONGO_URI = process.env.MONGO_URI;
 
 mongoose
   .connect(MONGO_URI)
-  .then((result) => {
+  .then(() => {
     console.log("Connected to MongoDB database successfully!");
   })
   .catch((error) => {
     console.log("Error connecting to MongoDB database:", error.message);
   });
 
-const personSchema = new mongoose.Schema({
-  name: String,
-  phoneNumber: String,
-});
+const personSchema = new mongoose.Schema(
+  {
+    name: String,
+    phoneNumber: String,
+  },
+  { collection: "people" }
+);
 
 personSchema.set("toJSON", {
   transform: (document, returnedObject) => {
